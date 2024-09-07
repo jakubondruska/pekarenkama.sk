599a951d412578f11787d6b1a078774c2380227b
Jakub Ondruska <jakubo.oondruska@gmail.com>
2024-09-07 19:36:42 +0200

    pridana fotogaleria

--- js/script.js
+++ js/script.js
@@ -1,44 +1,60 @@
 $(document).ready(function() {
-  // Inicializácia Isotope
-  var $grid = $('.collection-list').isotope({
-      itemSelector: '.col-lg-4',
-      layoutMode: 'fitRows',
-      filter: '.sladke' // Predvolený filter
-  });
+    var $grid = $('.collection-list').isotope({
+        itemSelector: '.col-lg-4',
+        layoutMode: 'fitRows',
+        filter: '.sladke' // Predvolený filter
+    });
 
-  // Aktivácia filtra
-  $('.filter-button-group').on('click', 'button', function() {
-      var filterValue = $(this).attr('data-filter');
-      resetFilterBtns();
-      $(this).addClass('active-filter-btn');
-      $grid.isotope({ filter: filterValue });
-  });
+    $('.filter-button-group').on('click', 'button', function() {
+        var filterValue = $(this).attr('data-filter');
+        resetFilterBtns();
+        $(this).addClass('active-filter-btn');
+        $grid.isotope({ filter: filterValue });
 
-  function resetFilterBtns() {
-      $('.filter-button-group button').removeClass('active-filter-btn');
-  }
+        // Zobrazovanie tlačidiel "Späť na kategórie"
+        showBackButtons(filterValue);
+    });
 
-  // Zatvorenie menu po kliknutí na položku
-  $('.navbar-nav .nav-link').on('click', function() {
-      var navbarToggler = document.querySelector('.navbar-toggler');
-      if (navbarToggler) {
-          var bsCollapse = new bootstrap.Collapse(document.querySelector('#navMenu'), {
-              toggle: false
-          });
-          bsCollapse.hide();
-      }
-  });
+    function resetFilterBtns() {
+        $('.filter-button-group button').removeClass('active-filter-btn');
+    }
 
-  // Zmena štýlu navbar pri skrolovaní
-  window.onscroll = function() {
-      var navbar = document.querySelector('.navbar');
-      var iconsSection = document.querySelector('#icons-section');
-      var iconsSectionBottom = iconsSection.offsetTop + iconsSection.offsetHeight;
+    function showBackButtons(filterValue) {
+        // Skryť všetky tlačidlá
+        $('.back-button').hide();
 
-      if (window.pageYOffset > iconsSectionBottom) {
-          navbar.classList.add('navbar-scrolled');
-      } else {
-          navbar.classList.remove('navbar-scrolled');
-      }
-  };
+        // Zobraziť tlačidlo len pre aktuálne filtrovanú kategóriu
+        if (filterValue !== '*') {
+            $('.back-button' + filterValue).show();
+        }
+    }
+
+    // Počiatočné zobrazenie tlačidiel
+    showBackButtons('.sladke'); // Predvolený filter
+
+    // Zatvorenie menu po kliknutí na položku
+    $('.navbar-nav .nav-link').on('click', function() {
+        var navbarToggler = document.querySelector('.navbar-toggler');
+        if (navbarToggler) {
+            var bsCollapse = new bootstrap.Collapse(document.querySelector('#navMenu'), {
+                toggle: false
+            });
+            bsCollapse.hide();
+        }
+    });
+
+    // Zmena štýlu navbar pri skrolovaní
+    window.onscroll = function() {
+        var navbar = document.querySelector('.navbar');
+        var iconsSection = document.querySelector('#icons-section');
+        var iconsSectionBottom = iconsSection.offsetTop + iconsSection.offsetHeight;
+
+        if (window.pageYOffset > iconsSectionBottom) {
+            navbar.classList.add('navbar-scrolled');
+        } else {
+            navbar.classList.remove('navbar-scrolled');
+        }
+    };
 });
+
+
